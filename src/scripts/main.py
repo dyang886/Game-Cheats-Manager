f720551019a9d48c0524e741b6da94ca82d727d3
Karasukaigan <80465610+Karasukaigan@users.noreply.github.com>
2025-03-27 15:28:15 +0800

    Update main.py

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -140,7 +140,7 @@ class GameCheatsManager(QMainWindow):
         searchSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="10" viewBox="0 0 512 512"><path fill="gray" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>'
         searchSvgWidget = QSvgWidget()
         searchSvgWidget.renderer().load(searchSvg.encode('utf-8'))
-        searchSvgWidget.setFixedSize(24, 24)
+        searchSvgWidget.setFixedSize(22, 22)
         trainerSearchLayout.addWidget(searchSvgWidget)
 
         self.trainerSearchEntry = QLineEdit()
@@ -181,7 +181,7 @@ class GameCheatsManager(QMainWindow):
 
         downloadSearchSvgWidget = QSvgWidget()
         downloadSearchSvgWidget.renderer().load(searchSvg.encode('utf-8'))
-        downloadSearchSvgWidget.setFixedSize(24, 24)
+        downloadSearchSvgWidget.setFixedSize(22, 22)
         downloadSearchLayout.addWidget(downloadSearchSvgWidget)
 
         self.downloadSearchEntry = QLineEdit()
